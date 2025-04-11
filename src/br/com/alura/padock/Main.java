5fc8c01329e47b5e22d82ad7efd54c8ffc338051
Angeliázer Rigon da Silva <yyrigon@hotmail.com>
2025-04-11 13:25:51 -0300

    second commit

--- src/br/com/alura/padock/Main.java
+++ src/br/com/alura/padock/Main.java
@@ -2,6 +2,6 @@ package br.com.alura.padock;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
